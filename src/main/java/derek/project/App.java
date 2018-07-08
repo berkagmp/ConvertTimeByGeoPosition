ce34a4b66dc079ad99ee7e470105428b651ad35d
berkagmp <berkagmp@gmail.com>
2018-07-08 18:06:06 +1200

    Modify with Lambda expression

--- src/main/java/derek/project/App.java
+++ src/main/java/derek/project/App.java
@@ -5,11 +5,12 @@ import static java.lang.System.out;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
-import java.util.TimeZone;
+import java.util.function.BiFunction;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.client.RestTemplate;
@@ -24,54 +25,54 @@ public class App {
 	public static void main(String[] args) {
 		String line = "";
 		String identifier = ",";
-		String timezone = "";
-		String output = "";
 		String filename = "data.csv";
+		StringBuilder output = new StringBuilder();
+		
+		BiFunction<String, String, String> f_timezone = (lat, lng) -> {
+			String url = "http://api.geonames.org/timezoneJSON?lat=" + lat + "&lng=" + lng + "&username="; //api_key
+
+			RestTemplate restTemplate = new RestTemplate();
+			String json = restTemplate.getForObject(url, String.class);
+
+			Gson gson = new Gson();
+			Map<String, String> result = gson.fromJson(json, new TypeToken<Map<String, String>>() {
+			}.getType());
+
+			return result.get("timezoneId");
+		};
+		
+		BiFunction<String, String, String> f_localTime = (str1, str2) -> {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+			LocalDateTime currentDT = LocalDateTime.parse(str1, formatter);
+			ZonedDateTime zonedDateTime = ZonedDateTime.of(currentDT, ZoneId.of("UTC"));
+			
+			formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+			
+			zonedDateTime = zonedDateTime.withZoneSameInstant(ZoneId.of(str2));
+			
+			return formatter.format(zonedDateTime);
+		};
 
 		try (BufferedReader br = new BufferedReader(
 				new FileReader(new ClassPathResource(filename).getFile().getAbsolutePath()))) {
 			while ((line = br.readLine()) != null) {
 				String[] arr = line.split(identifier);
-				// out.println(Arrays.asList(arr).toString());
 
 				for (int i = 0; i < arr.length; i++) {
-					output += arr[i] + identifier;
+					output.append(arr[i])
+							.append(identifier);
 				}
 
-				timezone = getTimezone(arr[1], arr[2]);
-				output += getTimezone(arr[1], arr[2]) + identifier + getLocalisedDatetime(arr[0], timezone);
-
+				String timezone = f_timezone.apply(arr[1], arr[2]);
+				output.append(timezone)
+						.append(identifier)
+						.append(f_localTime.apply(arr[0], timezone));
+				
 				out.println(output);
-				output = "";
+				output.setLength(0);
 			}
-		} catch (IOException | ParseException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-
-	public static String getLocalisedDatetime(String datetime, String timezone) throws ParseException {
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
-		Date date = formatter.parse(datetime);
-		String dTime = formatter.format(date);
-
-		formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-		formatter.setTimeZone(TimeZone.getTimeZone(timezone));
-		dTime = formatter.format(date);
-
-		return dTime;
-	}
-
-	public static String getTimezone(String lat, String lng) {
-		String url = "http://api.geonames.org/timezoneJSON?lat=" + lat + "&lng=" + lng + "&username="; //api_key
-
-		RestTemplate restTemplate = new RestTemplate();
-		String json = restTemplate.getForObject(url, String.class);
-
-		Gson gson = new Gson();
-		Map<String, String> result = gson.fromJson(json, new TypeToken<Map<String, String>>() {
-		}.getType());
-
-		return result.get("timezoneId");
 	}
 }
